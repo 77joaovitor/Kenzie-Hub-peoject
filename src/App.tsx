2892eb62bb8d3124eb41585dc07476b429b72024
77joaovitor <henrique.joaovitor@gmail.com>
2022-08-24 21:07:19 -0300

    feature:TS

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+import "./App.css";
+import ToastProvider from "./context/ToastContext";
+import AuthProvider from "./context/context";
+import AddProvider from "./context/AddContext";
+import RouterPages from "./Router/Router";
+import ToastContainer from "./components/ToastContainer";
+
+function App() {
+  return (
+    <div className="App">
+      <ToastProvider>
+        <AuthProvider>
+          <AddProvider>
+            <RouterPages />
+            <ToastContainer />
+          </AddProvider>
+        </AuthProvider>
+      </ToastProvider>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/Home/home.tsx
@@ -0,0 +1,51 @@
+import { useNavigate } from "react-router-dom";
+import { AuthContext } from "../../context/context";
+import { Header, Main } from "./style";
+import { useContext } from "react";
+import { Navigate } from "react-router-dom";
+import Loading from "../../components/loading/loading";
+import Cards from "../../components/cardList";
+import ModalTech from "../../components/modalAddTech";
+import { AddContext } from "../../context/AddContext";
+
+const HomePage = () => {
+  const navigate = useNavigate();
+  const { user, loading } = useContext(AuthContext);
+  const { modalAddTechVisible, setModalAddTechVisible } =
+    useContext(AddContext);
+  // const [modalUpdateTechVisible, setModalUpdateTechVisible] = useState(false);
+
+  function logOut() {
+    localStorage.removeItem("@context-user:token");
+    navigate("/");
+  }
+
+  if (loading) {
+    return <Loading />;
+  }
+  return user ? (
+    <>
+      {modalAddTechVisible && (
+        <ModalTech closeModalAddTech={() => setModalAddTechVisible(false)} />
+      )}
+      <Header>
+        <h1>Kenzie Hub</h1>
+        <button onClick={logOut}>Sair</button>
+      </Header>
+      <Main>
+        <div className="user__info">
+          <h1>{`Olá! ${user.name}`}</h1>
+          <span>{user.course_module}</span>
+        </div>
+        <Cards
+          openModalAddTech={() => setModalAddTechVisible(true)}
+          // openModalUpdateTech={() => setModalUpdateTechVisible(true)}
+        />
+      </Main>
+    </>
+  ) : (
+    <Navigate to={"/"} replace />
+  );
+};
+
+export default HomePage;
